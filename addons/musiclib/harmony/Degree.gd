961d4de626f3300d50b79b8611ea5b4cbdb511d4
laurentVeliscek <laurent.veliscek@gmail.com>
2025-12-05 05:11:55 +0100

    Add files via upload

--- addons/musiclib/harmony/Degree.gd
+++ addons/musiclib/harmony/Degree.gd
@@ -113,7 +113,7 @@ func clone()-> Degree:
 	d.degree_number = degree_number
 	d.realization = realization
 	d.kind = kind
-	d._alterations = _alterations
+	d._alterations = _alterations.duplicate()
 	d.inversion = inversion
 	d.length_beats = length_beats
 	d.harmonic_function = harmonic_function
@@ -221,11 +221,12 @@ func from_dict(data:Dictionary) -> Degree:
 	
 	if data.has("kind"):
 		d.set_kind(str(data["kind"]))
+		
 	
 	# Altérations accord
 	if data.has("alterations"):
 		var alt = data["alterations"]
-		if typeof(alt) == TYPE_DICTIONARY:
+		if typeof(alt) == TYPE_ARRAY:
 			d._set_alterations(alt.duplicate())
 	
 	if data.has("inversion"):
@@ -297,6 +298,7 @@ func from_dict(data:Dictionary) -> Degree:
 
 func _set_alterations(d:Dictionary = {}):
 	_alterations = d
+	LogBus.debug(TAG,"********** ALTERATION : "+ (str(_alterations)))
 
 func _get_alterations():
 	return _alterations
@@ -2024,7 +2026,9 @@ func guitar_chords()-> Array :
 	var keys = ["C","C#","D","Eb","E","F","F#","G","Ab","A","Bb","B"]
 	var gc_array = []
 	var basePitch =  key.degree_midi(degree_number) % 12
+	
 	var root_name = keys[basePitch]
+	var aug6_root_name = keys[ (key.degree_midi(1) + 8) % 12 ]
 	var triton_root_name = keys[(basePitch + 6) %12]
 	var backdoor_root_name = keys[(basePitch + 3) %12]
 	var chord_name = get_jazz_chord()
@@ -2036,10 +2040,12 @@ func guitar_chords()-> Array :
 		elif third_distance() == 4:
 			chord_name = root_name+"major"
 	elif kind == "It+6" or kind == "Fr+6" or kind == "Ger+6":
-		chord_name = root_name+"7"
+		chord_name = aug6_root_name+"7"
+		root_name = aug6_root_name
+		
 	elif kind == "It+6Inv" or kind == "Fr+6Inv" or kind == "Ger+6Inv":
-		chord_name = root_name+"7"
-	
+		chord_name = aug6_root_name+"7"
+		root_name = aug6_root_name
 	# search by name
 	gc_array = ggb.search_by_name(chord_name)
 	
@@ -2076,24 +2082,24 @@ func guitar_chords()-> Array :
 				gc_array.append_array(ggb.search_by_name(root_name+"9"))	
 
 	
-	# on ajoute les 6/9
-	if key.scale_name == "major":
-		if (degree_number == 1 or degree_number == 4 or degree_number == 5):
-			gc_array.append_array(ggb.search_by_name(root_name+"69"))
-		elif degree_number == 2:
-			gc_array.append_array(ggb.search_by_name(root_name+"m69"))
-	elif key.scale_name == "minor":
-		if (degree_number == 3 or degree_number == 6 or degree_number == 7):
-			gc_array.append_array(ggb.search_by_name(root_name+"69"))
-		elif degree_number == 4:
-			gc_array.append_array(ggb.search_by_name(root_name+"m69"))
-	elif  key.scale_name == "harmonic_minor":
-		if degree_number == 4:
-			gc_array.append_array(ggb.search_by_name(root_name+"m69"))
-	elif key.scale_name == "melodic_minor":
-		if degree_number == 4:
-			gc_array.append_array(ggb.search_by_name(root_name+"69"))
-				
+		# on ajoute les 6/9
+		if key.scale_name == "major":
+			if (degree_number == 1 or degree_number == 4 or degree_number == 5):
+				gc_array.append_array(ggb.search_by_name(root_name+"69"))
+			elif degree_number == 2:
+				gc_array.append_array(ggb.search_by_name(root_name+"m69"))
+		elif key.scale_name == "minor":
+			if (degree_number == 3 or degree_number == 6 or degree_number == 7):
+				gc_array.append_array(ggb.search_by_name(root_name+"69"))
+			elif degree_number == 4:
+				gc_array.append_array(ggb.search_by_name(root_name+"m69"))
+		elif  key.scale_name == "harmonic_minor":
+			if degree_number == 4:
+				gc_array.append_array(ggb.search_by_name(root_name+"m69"))
+		elif key.scale_name == "melodic_minor":
+			if degree_number == 4:
+				gc_array.append_array(ggb.search_by_name(root_name+"69"))
+					
 
 	# Et les Dominantes altérées
 	if kind == "diatonic" and realization == [1,3,5] and fifth_distance() == 7 and degree_number == 5:
